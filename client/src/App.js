27de6200ea587428e46d12a39e28452dd3254572
Dex <dxneufeld14@gmail.com>
2021-03-09 23:15:27 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import Playback from "./components/Playback";
+import "./styles/main.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+      <Route path="/" exact>
+        <main className="front-page">
+          <button className="front-page__button" onClick={() => window.location="http://localhost:8888/login"}>
+            Login with Spotify
+          </button>
+        </main>
+      </Route>
+        <Route path="/playback" component={Playback}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Playback.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route, Redirect, Link  } from "react-router-dom";
+
+class Playback extends React.Component{
+
+    render(){
+    return(
+    <div>
+        <h1>Playback Works</h1>
+    </div>
+    )
+}
+}
+export default Playback
